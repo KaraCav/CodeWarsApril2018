aa7723c51d5a06e25d26f326208ae59ad8ff8bfd
Kara Cavanaugh <karacav@msn.com>
2018-04-06 00:00:03 -0400

    Solution 5

--- scripts.js
+++ scripts.js
@@ -61,3 +61,13 @@ class SmallestIntegerFinder {
 }
 // ------------------ 110,675 -------------------------------- //
 
+//NUMBER 5: Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+function solution(nums) {
+    if (nums == null || nums.length == 0) {
+        return [];
+    }
+    nums.sort(function (a, b) {
+        return a - b;
+    });
+    return nums;
+}
