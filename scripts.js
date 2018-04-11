d5aad15e51d223b9c38304759cab4797b7538af1
Kara Cavanaugh <karacav@msn.com>
2018-04-10 20:03:53 -0400

    Solution 8

--- scripts.js
+++ scripts.js
@@ -86,3 +86,21 @@ function duplicateCount(text) {
 function isDivisible(n, x, y) {
     return (n % x === 0 && n % y === 0);
 }
+// NUMBER 8: You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'. You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So: 'C.....m' returns 'Escaped!' < --more than three characters between. 'C...m' returns 'Caught!'
+function catMouse(x) {
+    var myArray = x.split('');
+    var count = 0;
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] == '.') {
+            count += 1;
+        }
+    }
+    if (count > 3) {
+        return "Escaped!"
+    }
+    else { return 'Caught!' }
+}
+//OR can do:
+function catMouse(x) {
+    return x.length <= 5 ? 'Caught!' : 'Escaped!';
+}
